c7429adfa77cb666d943995d9e0883a7c9971cd5
Luiz Fonseca <runeroniek@gmail.com>
2011-10-08 20:12:38 -0400

    Just testing

--- recipe.py
+++ recipe.py
@@ -1,15 +1,11 @@
 # -*- coding: utf-8 -*-
 import os
 import cherrypy
-import urllib
-
-
-
 
 class Recipe:
   @cherrypy.expose
   def index(self):
-    return "Natasha <3"
+    return "It's working"
 
 
 cherrypy.quickstart(Recipe())
